1bff0a24c20f68fb13a756fd3e468f1dc0575527
Matt Ao <mattyayoh@gmail.com>
2014-02-10 17:54:27 -0500

    Added score activity and landscape for main

--- gen/edu/msu/myturns/stackergame/R.java
+++ gen/edu/msu/myturns/stackergame/R.java
@@ -34,18 +34,21 @@ public final class R {
         public static final int stacker=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int imageView1=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int anchor=0x7f080003;
+        public static final int stackerImage=0x7f080000;
+        public static final int startButton=0x7f080001;
+        public static final int tutorialButton=0x7f080002;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_score=0x7f030002;
     }
     public static final class menu {
         public static final int game=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int score=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -54,6 +57,7 @@ public final class R {
         public static final int stacker_description=0x7f050006;
         public static final int start=0x7f050004;
         public static final int title_activity_game=0x7f050003;
+        public static final int title_activity_score=0x7f050007;
         public static final int tutorial=0x7f050005;
     }
     public static final class style {
